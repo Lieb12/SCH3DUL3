ad56b8cac8786d9711f031fabb717c9d4a7c0a2e
Owen Lieberman <lieberman03@gmail.com>
2020-09-23 18:46:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+
+const client = new Discord.Client();
+const prefix = "!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command == "botprogress") {
+    message.channel.send("Check back on Friday!")
+  } else if (command == "teams") {
+    message.channel.send("Teams: Writing, Art, Coding, Modelling/Anim, Misc.")
+  } else if (command == "") {
+    
+  }
+});
+
+client.login(config.BOT_TOKEN);
